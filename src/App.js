004609ec91eb86b69f8d49e1a94607bc13f437ee
Tirth Gajjar <tirth.g@applcoum.org>
2024-08-30 14:51:29 +0530

    first commit-1

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Testing React
         </a>
         <button>Click me</button>
       </header>
